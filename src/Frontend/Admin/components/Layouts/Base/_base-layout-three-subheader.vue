038d85fc553a74c66507d92f505877c6c7533b3a
Rubal Gulati <rubal@noetic.com>
2019-06-20 17:41:35 +0530

    top header

--- src/Frontend/Admin/components/Layouts/Base/_base-layout-three-subheader.vue
+++ src/Frontend/Admin/components/Layouts/Base/_base-layout-three-subheader.vue
@@ -3,11 +3,11 @@
     <div class="kt-subheader   kt-grid__item" id="kt_subheader">
         <div class="kt-subheader__main">
             <h3 class="kt-subheader__title">Dashboard</h3>
-            <span class="kt-subheader__separator kt-subheader__separator--v"></span>
-            <span class="kt-subheader__desc">#XRS-45670</span>
-            <a href="#" class="btn btn-label-warning btn-bold btn-sm btn-icon-h kt-margin-l-10">
-                Add New
-            </a>
+<!--            <span class="kt-subheader__separator kt-subheader__separator&#45;&#45;v"></span>-->
+<!--            <span class="kt-subheader__desc">#XRS-45670</span>-->
+<!--            <a href="#" class="btn btn-label-warning btn-bold btn-sm btn-icon-h kt-margin-l-10">-->
+<!--                Add New-->
+<!--            </a>-->
             <div class="kt-input-icon kt-input-icon--right kt-subheader__search kt-hidden">
                 <input type="text" class="form-control" placeholder="Search order..." id="generalSearch">
                 <span class="kt-input-icon__icon kt-input-icon__icon--right">
@@ -17,12 +17,12 @@
         </div>
         <div class="kt-subheader__toolbar">
             <div class="kt-subheader__wrapper">
-                <a href="#" class="btn kt-subheader__btn-secondary">Today</a>
-                <a href="#" class="btn kt-subheader__btn-secondary">Month</a>
-                <a href="#" class="btn kt-subheader__btn-secondary">Year</a>
+<!--                <a href="#" class="btn kt-subheader__btn-secondary">Today</a>-->
+<!--                <a href="#" class="btn kt-subheader__btn-secondary">Month</a>-->
+<!--                <a href="#" class="btn kt-subheader__btn-secondary">Year</a>-->
                 <a href="#" class="btn kt-subheader__btn-daterange" id="kt_dashboard_daterangepicker" data-toggle="kt-tooltip" title="Select dashboard daterange" data-placement="left">
                     <span class="kt-subheader__btn-daterange-title" id="kt_dashboard_daterangepicker_title">Today</span>&nbsp;
-                    <span class="kt-subheader__btn-daterange-date" id="kt_dashboard_daterangepicker_date">Aug 16</span>
+                    <span class="kt-subheader__btn-daterange-date" id="kt_dashboard_daterangepicker_date">June 20</span>
                     <i class="flaticon2-calendar-1"></i>
                 </a>
                 <div class="dropdown dropdown-inline" data-toggle="kt-tooltip" title="Quick actions" data-placement="left">
