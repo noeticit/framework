686d1f83fd1b3434cc1db2054c4adde9819d7531
Rubal Gulati <rubal@noetic.com>
2019-07-23 16:28:43 +0530

    Route fix

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,7 +1,1 @@
 <?php
-
-Route::get('/', function () {
-    return view('nitseditor::Website.welcome');
-});
-
-Route::get('/admin/{view?}', 'HomeController@admin')->where('view', '(.*)')->name('nitseditor.admin');
