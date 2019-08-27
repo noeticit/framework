ea00d6c091b33ee146f0c2bb2d579c9b6d2593e9
Nitish Kumar <nitishkumar@noeticitservices.com>
2019-08-27 19:29:37 +0530

    Calendar events props

--- src/Frontend/Admin/components/Elements/Calendar/Calendar.vue
+++ src/Frontend/Admin/components/Elements/Calendar/Calendar.vue
@@ -6,7 +6,6 @@
     import {KTApp} from 'NitsTheme/framework/lib/app';
     import {KTUtil} from 'NitsTheme/framework/lib/util';
 
-    var moment = require('moment');
     require('fullcalendar');
 
     export default {
@@ -21,11 +20,6 @@
                 return {
                     //main function to initiate the module
                     init: function() {
-                        var todayDate = moment().startOf('day');
-                        var YM = todayDate.format('YYYY-MM');
-                        var YESTERDAY = todayDate.clone().subtract(1, 'day').format('YYYY-MM-DD');
-                        var TODAY = todayDate.format('YYYY-MM-DD');
-                        var TOMORROW = todayDate.clone().add(1, 'day').format('YYYY-MM-DD');
 
                         $('#'+self.identity).fullCalendar({
                             isRTL: KTUtil.isRTL(),
@@ -39,102 +33,7 @@
                             eventLimit: true, // allow "more" link when too many events
                             navLinks: true,
                             height: 900,
-                            events: [
-                                {
-                                    title: 'All Day Event',
-                                    start: YM + '-01',
-                                    description: 'Lorem ipsum dolor sit incid idunt ut',
-                                    className: "fc-event-success"
-                                },
-                                {
-                                    title: 'Reporting',
-                                    start: YM + '-14T13:30:00',
-                                    description: 'Lorem ipsum dolor incid idunt ut labore',
-                                    end: YM + '-14',
-                                    className: "fc-event-accent"
-                                },
-                                {
-                                    title: 'Company Trip',
-                                    start: YM + '-02',
-                                    description: 'Lorem ipsum dolor sit tempor incid',
-                                    end: YM + '-03',
-                                    className: "fc-event-primary"
-                                },
-                                {
-                                    title: 'Expo',
-                                    start: YM + '-03',
-                                    description: 'Lorem ipsum dolor sit tempor inci',
-                                    end: YM + '-05',
-                                    className: "fc-event-primary"
-                                },
-                                {
-                                    title: 'Dinner',
-                                    start: YM + '-12',
-                                    description: 'Lorem ipsum dolor sit amet, conse ctetur',
-                                    end: YM + '-10'
-                                },
-                                {
-                                    id: 999,
-                                    title: 'Repeating Event',
-                                    start: YM + '-09T16:00:00',
-                                    description: 'Lorem ipsum dolor sit ncididunt ut labore',
-                                    className: "fc-event-danger"
-                                },
-                                {
-                                    id: 1000,
-                                    title: 'Repeating Event',
-                                    description: 'Lorem ipsum dolor sit amet, labore',
-                                    start: YM + '-16T16:00:00'
-                                },
-                                {
-                                    title: 'Conference',
-                                    start: YESTERDAY,
-                                    end: TOMORROW,
-                                    description: 'Lorem ipsum dolor eius mod tempor labore',
-                                    className: "fc-event-accent"
-                                },
-                                {
-                                    title: 'Meeting',
-                                    start: TODAY + 'T10:30:00',
-                                    end: TODAY + 'T12:30:00',
-                                    description: 'Lorem ipsum dolor eiu idunt ut labore'
-                                },
-                                {
-                                    title: 'Lunch',
-                                    start: TODAY + 'T12:00:00',
-                                    className: "fc-event-info",
-                                    description: 'Lorem ipsum dolor sit amet, ut labore'
-                                },
-                                {
-                                    title: 'Meeting',
-                                    start: TODAY + 'T14:30:00',
-                                    className: "fc-event-warning",
-                                    description: 'Lorem ipsum conse ctetur adipi scing'
-                                },
-                                {
-                                    title: 'Happy Hour',
-                                    start: TODAY + 'T17:30:00',
-                                    className: "fc-event-metal",
-                                    description: 'Lorem ipsum dolor sit amet, conse ctetur'
-                                },
-                                {
-                                    title: 'Dinner',
-                                    start: TODAY + 'T20:00:00',
-                                    description: 'Lorem ipsum dolor sit ctetur adipi scing'
-                                },
-                                {
-                                    title: 'Birthday Party',
-                                    start: TOMORROW + 'T07:00:00',
-                                    className: "fc-event-primary",
-                                    description: 'Lorem ipsum dolor sit amet, scing'
-                                },
-                                {
-                                    title: 'Click for Google',
-                                    url: 'http://google.com/',
-                                    start: YM + '-28',
-                                    description: 'Lorem ipsum dolor sit amet, labore'
-                                }
-                            ],
+                            events: self.events,
 
                             eventRender: function(event, element) {
                                 if (element.hasClass('fc-day-grid-event')) {
