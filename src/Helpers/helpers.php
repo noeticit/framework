1cc28f12f3f745e745b56904cf4d92baf197f6fd
Nitish Kumar <nitish1986@users.noreply.github.com>
2019-11-11 13:42:18 +0530

    str_contains() helper fix

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 if (! function_exists('nits_plugins')) {
     /**
@@ -90,7 +91,7 @@ if(! function_exists('document_s3_upload')) {
 
         $decoded = base64_decode($exploded[1]);
 
-        if (str_contains($exploded[0], $format[1])) {
+        if (Str::contains($exploded[0], $format[1])) {
             $extension = $format[1];
         }
 
